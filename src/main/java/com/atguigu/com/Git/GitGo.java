5f95f989672122fa2181ad4e9cd1bda6188d0594
Zouzy <1598322981@qq.com>
2020-12-15 00:29:03 +0800

    修改项目Git_2.0--Zouzy

--- src/main/java/com/atguigu/com/Git/GitGo.java
+++ src/main/java/com/atguigu/com/Git/GitGo.java
@@ -6,5 +6,8 @@ public class GitGo{
         System.out.println("好好学习,天天向上");
         System.out.println("Zouzy");
         System.out.println("one");
+        System.out.println("two");
+        System.out.println("there");
+        System.out.println("four");
     }
 }
