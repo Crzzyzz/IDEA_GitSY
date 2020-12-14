44f34902ca0fa71a59cf286e9727b509840ad622
CXYZZY <zzy1598322981@163.com>
2020-12-15 00:32:44 +0800

    修改并提交,Git_2.5--CXYZZY

--- src/main/java/com/atguigu/com/Git/GitGo.java
+++ src/main/java/com/atguigu/com/Git/GitGo.java
@@ -4,7 +4,11 @@ public class GitGo{
     public static void main (String[] args){
         System.out.println("从百草园到三味书屋");
         System.out.println("好好学习,天天向上");
-        System.out.println("Zouzy");
+        System.out.println("CXYZZY");
         System.out.println("one");
+        System.out.println("AAA");
+        System.out.println("BBB");
+        System.out.println("CCC");
+        System.out.println("DDD");
     }
 }
