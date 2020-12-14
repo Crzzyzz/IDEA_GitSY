6054f0edcd9b8c8c02f55b42b1e46a8e748c6eb2
Zouzy <1598322981@qq.com>
2020-12-14 23:38:35 +0800

    创建项目Git_1.0

--- src/main/java/com/atguigu/com/Git/GitGo.java
+++ src/main/java/com/atguigu/com/Git/GitGo.java
@@ -4,5 +4,6 @@ public class GitGo{
     public static void main (String[] args){
         System.out.println("从百草园到三味书屋");
         System.out.println("好好学习,天天向上");
+        System.out.println("Zouzy");
     }
 }
